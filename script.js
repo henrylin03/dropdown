b7fcdb08e410c56d26dc80e8415c4eb0cdbce4cb
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-27 12:57:03 +1000

    on selection of language, the current selected language changes

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const dropdownBtn = document.querySelector(".language-selector-btn");
-const dropdownOptions = document.querySelector(".dropdown-options");
+const dropdownOptionsContainer = document.querySelector(".dropdown-options");
+const dropdownOptions = document.querySelectorAll(".option");
 const currentLanguage = document.querySelector(".selected-language");
 
 dropdownBtn.addEventListener("mousedown", () =>
-  dropdownOptions.classList.toggle("opened")
+  dropdownOptionsContainer.classList.toggle("opened")
 );
 dropdownBtn.addEventListener("blur", () =>
-  dropdownOptions.classList.remove("opened")
+  dropdownOptionsContainer.classList.remove("opened")
+);
+dropdownOptions.forEach((elem) =>
+  elem.addEventListener("mousedown", () => {
+    const language = elem.querySelector("p").textContent;
+    currentLanguage.textContent = language;
+  })
 );
