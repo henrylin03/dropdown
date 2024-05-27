b474c6dd1c0393bee46c78c23faac3737fa026cb
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-28 06:52:19 +1000

    add option for the button's own text be replaced with a selection. return error if the type is not a boolean. early return if error for expandEvent too

--- src/createDropdown.js
+++ src/createDropdown.js
@@ -2,16 +2,21 @@ export default function createDropdown({
   toggleBtnSelector,
   optionsSelector,
   expandEvent,
+  replaceTextWithSelection,
 }) {
   // select
   const toggleBtnElement = document.querySelector(toggleBtnSelector);
   const optionsNodes = document.querySelectorAll(optionsSelector);
   const VALID_EXPAND_EVENTS = ["hover", "click", "mousedown"];
   if (!expandEvent || !VALID_EXPAND_EVENTS.includes(expandEvent))
-    console.error(
+    return console.error(
       "Please use a valid event type for expanding the dropdown. Valid events are 'hover', 'click', 'mousedown'."
     );
   const expandEventType = expandEvent === "hover" ? "hover" : "mousedown";
+  if (typeof replaceTextWithSelection != "boolean")
+    return console.error(
+      "Please ensure only boolean (true or false) value for the 'replaceTextWithSelector' option."
+    );
 
   // method
   const toggleOptions = () => toggleBtnElement.classList.toggle("opened");
@@ -23,3 +28,5 @@ export default function createDropdown({
 
   return;
 }
+
+//todo: in docs, explain what each parameter means, expected values, and show examples!
