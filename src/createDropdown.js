2cddc3e536d342977e243c78a8466c58b0619e9c
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-28 06:35:53 +1000

    add check for also whether expandEvent is null - return same console error as an invalid expandEvent arg (destructured). clean event type - if user selects 'click', they will actually get mousedown

--- src/createDropdown.js
+++ src/createDropdown.js
@@ -8,11 +8,11 @@ export default function createDropdown({
   const toggleBtnElement = document.querySelector(toggleBtnSelector);
   const optionsNodes = document.querySelectorAll(optionsSelector);
 
-  // run
-  if (!VALID_EXPAND_EVENTS.includes(expandEvent))
+  if (!expandEvent || !VALID_EXPAND_EVENTS.includes(expandEvent))
     console.error(
       "Please use a valid event type for expanding the dropdown. Valid events are 'hover', 'click', 'mousedown'."
     );
+  const expandEventType = expandEvent === "hover" ? "hover" : "mousedown";
 
   return;
 }
