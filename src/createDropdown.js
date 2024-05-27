a05556f17803a9ac8b487495078f94f19811508c
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-28 06:39:27 +1000

    add methods for event handling

--- src/createDropdown.js
+++ src/createDropdown.js
@@ -3,16 +3,19 @@ export default function createDropdown({
   optionsSelector,
   expandEvent,
 }) {
-  const VALID_EXPAND_EVENTS = ["hover", "click", "mousedown"];
-
+  // select
   const toggleBtnElement = document.querySelector(toggleBtnSelector);
   const optionsNodes = document.querySelectorAll(optionsSelector);
-
+  const VALID_EXPAND_EVENTS = ["hover", "click", "mousedown"];
   if (!expandEvent || !VALID_EXPAND_EVENTS.includes(expandEvent))
     console.error(
       "Please use a valid event type for expanding the dropdown. Valid events are 'hover', 'click', 'mousedown'."
     );
   const expandEventType = expandEvent === "hover" ? "hover" : "mousedown";
+
+  // method
+  const toggleOptions = () => toggleBtnElement.classList.toggle("opened");
+  const hideOptions = () => toggleBtnElement.classList.remove("opened");
 
   return;
 }
