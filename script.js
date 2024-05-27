bb4709de5a002d1f5e5b5e3389394f563e1da56e
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-27 12:52:02 +1000

    add selected language into options as well. add slight padding to give a ring around options

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dropdownBtn = document.querySelector(".language-selector-btn");
 const dropdownOptions = document.querySelector(".dropdown-options");
+const currentLanguage = document.querySelector(".selected-language");
 
 dropdownBtn.addEventListener("mousedown", () =>
   dropdownOptions.classList.toggle("opened")
