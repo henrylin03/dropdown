bd073876986970b92582b0ebb2526665ee1b1829
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-29 16:22:21 +1000

    get the currently selected language (text in the button that triggers dropdown) as a dom element ready for manipulation. update that language with what has been selected by user

--- src/createDropdown.js
+++ src/createDropdown.js
@@ -4,10 +4,12 @@
 
 export default function createDropdown({
   toggleBtnSelector = ".dropdown .language-selector-btn",
+  toggleBtnTextSelector = ".current-language",
   optionsContainerSelector = ".dropdown .dropdown-options",
   expandEvent = "click",
 } = {}) {
   const toggleBtnElement = document.querySelector(toggleBtnSelector);
+  const currentLanguage = document.querySelector(toggleBtnTextSelector);
   const optionsContainerElement = document.querySelector(
     optionsContainerSelector
   );
@@ -23,8 +25,11 @@ export default function createDropdown({
   const toggleOptions = () =>
     optionsContainerElement.classList.toggle("opened");
   const hideOptions = () => optionsContainerElement.classList.remove("opened");
-  const updateBtnTextWithChosenOption = (e) =>
-    console.log(e.target.querySelector("p"));
+  const updateBtnTextWithChosenOption = (e) => {
+    const listElem = e.target.closest("li");
+    const chosenLanguage = listElem.querySelector("p").textContent;
+    currentLanguage.textContent = chosenLanguage;
+  };
 
   // attach event listeners
   toggleBtnElement.addEventListener(expandEventType, toggleOptions);
