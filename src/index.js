5d4bfbffc3967d224e06fc1335e698dc999894e6
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-30 08:34:32 +1000

    create sections to demonstrate click and hover dropdowns. add title

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import "./assets/styles/global.css";
 import createDropdown from "./createDropdown";
 
 const LANGUAGES_CHINESE = ["English", "简体中文", "繁體中文"];
-createDropdown({ expandEvent: "click", languages: LANGUAGES_CHINESE });
-createDropdown({ expandEvent: "hover", languages: LANGUAGES_CHINESE });
+const LANGUAGES_UN = ["English", "简体中文"];
+
+createDropdown({
+  containerElementCSSSelector: "#click-dropdown",
+  expandEvent: "click",
+  languages: LANGUAGES_CHINESE,
+});
+createDropdown({
+  containerElementCSSSelector: "#hover-dropdown",
+  expandEvent: "hover",
+  languages: LANGUAGES_CHINESE,
+});
